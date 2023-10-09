1e98a2054009c1a1dd080420b05657b42578b64e
ROGER MESTANZA <rmestanza.217@gmail.com>
2023-10-09 13:20:41 -0500

    add last changes

--- rest/src/main/java/org/pe/api/rest/RestApplication.java
+++ rest/src/main/java/org/pe/api/rest/RestApplication.java
@@ -1,13 +1,29 @@
 package org.pe.api.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class RestApplication {
 
+	@Value("${server.port}")
+	private  String port;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestApplication.class, args);
+		Properties sysProperties = System.getProperties();
+		String port = sysProperties.getProperty("server.port");
+		System.out.println("Listening on port: " + port);
+
+	}
+
+	public  String showPort() {
+		return port;
+
 	}
 
 }
