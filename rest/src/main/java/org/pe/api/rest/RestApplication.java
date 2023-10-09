530030b721506dfc3272307c577981cf8f87805e
ROGER MESTANZA <rmestanza.217@gmail.com>
2023-10-09 13:34:27 -0500

    save last changes
    	# Please enter the commit message for your changes. Lines starting

--- rest/src/main/java/org/pe/api/rest/RestApplication.java
+++ rest/src/main/java/org/pe/api/rest/RestApplication.java
@@ -15,11 +15,7 @@ public class RestApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(RestApplication.class, args);
-<<<<<<< HEAD
-		Properties p = System.getProperties();
-		String port = p.getProperty("server.port");
-		System.out.println("Listening on port: " + port);
-=======
+
 		Properties sysProperties = System.getProperties();
 		String port = sysProperties.getProperty("server.port");
 		System.out.println("Listening on port: " + port);
@@ -29,7 +25,6 @@ public class RestApplication {
 	public  String showPort() {
 		return port;
 
->>>>>>> 1e98a2054009c1a1dd080420b05657b42578b64e
 	}
 
 }
